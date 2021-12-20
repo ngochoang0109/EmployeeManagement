81c301b5b0623d86e9585e9de2d561c52ae0cd03
NguyenVanPhap <nguyenvanphap013@gmail.com>
2021-12-20 22:23:21 +0700

    add login function

--- src/main/java/com/employeemanagement/app/EmployeeManagementApplication.java
+++ src/main/java/com/employeemanagement/app/EmployeeManagementApplication.java
@@ -2,12 +2,16 @@ package com.employeemanagement.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.employeemanagement.app.controller", "com.employeemanagement.app" })
+@ControllerAdvice
 public class EmployeeManagementApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EmployeeManagementApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApplication.class, args);
+	}
 
 }
--- src/main/java/com/employeemanagement/app/service/EmployeeService.java
+++ src/main/java/com/employeemanagement/app/service/EmployeeService.java
@@ -11,39 +11,53 @@ import org.springframework.stereotype.Service;
 import com.employeemanagement.app.dao.EmployeeDA;
 import com.employeemanagement.app.entities.Employee;
 import com.employeemanagement.app.helpers.ApiRes;
+import com.employeemanagement.app.helpers.ValidationHelper.Result;
 import com.employeemanagement.app.request.EmployeeReq;
 
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
+import com.employeemanagement.app.service.*;
 
 @Service
 public class EmployeeService {
 
-    @Autowired
-    private EmployeeDA employeeDA;
+	@Autowired
+	private HandlerBL handlerBL;
+	@Autowired
+	private EmployeeDA employeeDA;
 
-    public ApiRes<Object> getlist() {
-        ApiRes<Object> apiRes = new ApiRes<Object>();
-        try {
-            List<Employee> employees = employeeDA.getList("");
-            apiRes.setObject(employees);
-        } catch (Exception e) {
-            apiRes.setError(true);
-            apiRes.setErrorReason(e.getMessage());
-        }
-        return apiRes;
-    }
+	public ApiRes<Object> getlist() {
+		ApiRes<Object> apiRes = new ApiRes<Object>();
+		try {
+			List<Employee> employees = employeeDA.getList("");
+			apiRes.setObject(employees);
+		} catch (Exception e) {
+			apiRes.setError(true);
+			apiRes.setErrorReason(e.getMessage());
+		}
+		return apiRes;
+	}
 
-    public ResponseEntity<Object> login(LoginReq req, HttpSession session) {
-        DataSource dataSource = employeeDA.connect(req);
-        if (dataSource == null)
-            return ResponseEntity.status(401).body("Kiem tra lai tai khoan hoac mat khau");
-        session.setAttribute("datasource", dataSource);
-        return ResponseEntity.ok().body("Connect success");
-    }
+	public ApiRes<Object> login(LoginReq req, HttpSession session) {
+		ApiRes<Object> apiRes = new ApiRes<Object>();
+		try {
+			DataSource dataSource = employeeDA.connect(req);
+			if (dataSource == null)
+				apiRes = handlerBL.translateError(Result.login_fail, "Đăng nhập thất bại");
+			else {
+				session.setAttribute("datasource", dataSource);
+				apiRes.setObject(true);
+				apiRes.setError(false);
+			}
+		} catch (Exception e) {
+			apiRes = handlerBL.translateError(Result.login_fail, "Đăng nhập thất bại");
+		}
+		return apiRes;
 
-    public ResponseEntity<Object> logout(HttpSession session) {
-        session.removeAttribute("dataSource");
-        return ResponseEntity.ok().body("logout success");
-    }
+	}
+
+	public ResponseEntity<Object> logout(HttpSession session) {
+		session.removeAttribute("dataSource");
+		return ResponseEntity.ok().body("logout success");
+	}
 }
