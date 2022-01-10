29359535059bf03599247949bb550c0b4f4d7b0d
dmanhhieu <hieudoanmanh.it@gmail.com>
2022-01-10 23:28:36 +0700

    audit

--- src/main/java/com/employeemanagement/app/controller/EmployeeController.java
+++ src/main/java/com/employeemanagement/app/controller/EmployeeController.java
@@ -5,90 +5,100 @@ import com.employeemanagement.app.helpers.ApiRes;
 import com.employeemanagement.app.request.EmployeeReq;
 import com.employeemanagement.app.request.FilterReq;
 import com.employeemanagement.app.request.LoginReq;
+import com.employeemanagement.app.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.employeemanagement.app.service.EmployeeService;
 
 import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/employee")
 public class EmployeeController {
-	@Autowired
-	private EmployeeService objBL;
+    @Autowired
+    private EmployeeService objBL;
 
-	@RequestMapping(value = "/getlist", method = RequestMethod.POST, consumes = "application/json", produces = "application/json; charset=utf-8")
-	public ResponseEntity<Object> getList(HttpSession session, @RequestBody FilterReq req) throws Exception {
-		if (!EmployeeDA.GetDataSource(session)) {
-			ApiRes<Object> apiRes = new ApiRes<Object>();
-			apiRes.setError(true);
-			apiRes.setErrorReason("Vui lòng đăng nhập");
-			return ResponseEntity.ok(apiRes);
-		}
-		return ResponseEntity.ok(objBL.getlist(req));
-	}
+    @RequestMapping(value = "/getlist", method = RequestMethod.POST, consumes = "application/json", produces = "application/json; charset=utf-8")
+    public ResponseEntity<Object> getList(HttpSession session, @RequestBody FilterReq req) throws Exception {
+        if (!EmployeeDA.GetDataSource(session)) {
+            ApiRes<Object> apiRes = new ApiRes<Object>();
+            apiRes.setError(true);
+            apiRes.setErrorReason("Vui lòng đăng nhập");
+            return ResponseEntity.ok(apiRes);
+        }
+        return ResponseEntity.ok(objBL.getlist(req));
+    }
 
-	@RequestMapping(value = "/getinfor", method = RequestMethod.POST, consumes = "application/json", produces = "application/json; charset=utf-8")
-	public ResponseEntity<Object> getInfor(HttpSession session, @RequestBody EmployeeReq req) throws Exception {
-		if (!EmployeeDA.GetDataSource(session)) {
-			ApiRes<Object> apiRes = new ApiRes<Object>();
-			apiRes.setError(true);
-			apiRes.setErrorReason("Vui lòng đăng nhập");
-			return ResponseEntity.ok(apiRes);
-		}
-		return ResponseEntity.ok(objBL.getInfor(req));
-	}
+    @RequestMapping(value = "/getinfor", method = RequestMethod.POST, consumes = "application/json", produces = "application/json; charset=utf-8")
+    public ResponseEntity<Object> getInfor(HttpSession session, @RequestBody EmployeeReq req) throws Exception {
+        if (!EmployeeDA.GetDataSource(session)) {
+            ApiRes<Object> apiRes = new ApiRes<Object>();
+            apiRes.setError(true);
+            apiRes.setErrorReason("Vui lòng đăng nhập");
+            return ResponseEntity.ok(apiRes);
+        }
+        return ResponseEntity.ok(objBL.getInfor(req));
+    }
 
-	@RequestMapping(value = "/add", method = RequestMethod.POST, consumes = "application/json", produces = "application/json; charset=utf-8")
-	public ResponseEntity<Object> add(HttpSession session, @RequestBody EmployeeReq req) throws Exception {
-		if (!EmployeeDA.GetDataSource(session)) {
-			return ResponseEntity.status(400).body("Bad request");
-		}
-		return ResponseEntity.ok(objBL.add(req));
-	}
+    @RequestMapping(value = "/add", method = RequestMethod.POST, consumes = "application/json", produces = "application/json; charset=utf-8")
+    public ResponseEntity<Object> add(HttpSession session, @RequestBody EmployeeReq req) throws Exception {
+        if (!EmployeeDA.GetDataSource(session)) {
+            return ResponseEntity.status(400).body("Bad request");
+        }
+        return ResponseEntity.ok(objBL.add(req));
+    }
 
-	@PostMapping("login")
-	public ResponseEntity<Object> login(@RequestBody LoginReq req, HttpSession session) {
-		return ResponseEntity.ok(objBL.login(req, session));
-	}
+    @PostMapping("login")
+    public ResponseEntity<Object> login(@RequestBody LoginReq req, HttpSession session) {
+        return ResponseEntity.ok(objBL.login(req, session));
+    }
 
-	@GetMapping("logout")
-	public ResponseEntity<Object> logout(HttpSession session) {
-		return ResponseEntity.ok(objBL.logout(session));
-	}
+    @GetMapping("logout")
+    public ResponseEntity<Object> logout(HttpSession session) {
+        return ResponseEntity.ok(objBL.logout(session));
+    }
 
-	@RequestMapping(value = "/delete", method = RequestMethod.POST, consumes = "application/json", produces = "application/json; charset=utf-8")
-	public ResponseEntity<Object> delete(@RequestBody EmployeeReq employeeReq, HttpSession session) {
-		if (!EmployeeDA.GetDataSource(session)) {
-			ApiRes<Object> apiRes = new ApiRes<Object>();
-			apiRes.setError(true);
-			apiRes.setErrorReason("Vui lòng đăng nhập");
-			return ResponseEntity.ok(apiRes);
-		}
-		return ResponseEntity.ok(objBL.delEmp(employeeReq.getId()));
-	}
+    @RequestMapping(value = "/delete", method = RequestMethod.POST, consumes = "application/json", produces = "application/json; charset=utf-8")
+    public ResponseEntity<Object> delete(@RequestBody EmployeeReq employeeReq, HttpSession session) {
+        if (!EmployeeDA.GetDataSource(session)) {
+            ApiRes<Object> apiRes = new ApiRes<Object>();
+            apiRes.setError(true);
+            apiRes.setErrorReason("Vui lòng đăng nhập");
+            return ResponseEntity.ok(apiRes);
+        }
+        return ResponseEntity.ok(objBL.delEmp(employeeReq.getId()));
+    }
 
-	/*
-	 * @RequestMapping(value = "/getinfor", method = RequestMethod.POST, consumes =
-	 * "application/json", produces = "application/json; charset=utf-8") public
-	 * ResponseEntity<Object> getEmployee(@RequestBody EmployeeReq employeeReq,
-	 * HttpSession session) { if (!EmployeeDA.GetDataSource(session)) {
-	 * ApiRes<Object> apiRes = new ApiRes<Object>(); apiRes.setError(true);
-	 * apiRes.setErrorReason("Vui lòng đăng nhập"); return
-	 * ResponseEntity.ok(apiRes); } return
-	 * ResponseEntity.ok(objBL.getEmpById(employeeReq.getId())); }
-	 */
+    /*
+     * @RequestMapping(value = "/getinfor", method = RequestMethod.POST, consumes =
+     * "application/json", produces = "application/json; charset=utf-8") public
+     * ResponseEntity<Object> getEmployee(@RequestBody EmployeeReq employeeReq,
+     * HttpSession session) { if (!EmployeeDA.GetDataSource(session)) {
+     * ApiRes<Object> apiRes = new ApiRes<Object>(); apiRes.setError(true);
+     * apiRes.setErrorReason("Vui lòng đăng nhập"); return
+     * ResponseEntity.ok(apiRes); } return
+     * ResponseEntity.ok(objBL.getEmpById(employeeReq.getId())); }
+     */
 
-	@RequestMapping(value = "/update", method = RequestMethod.POST, consumes = "application/json", produces = "application/json; charset=utf-8")
-	public ResponseEntity<Object> updateEmp(@RequestBody EmployeeReq employeeReq, HttpSession session) {
-		if (!EmployeeDA.GetDataSource(session)) {
-			ApiRes<Object> apiRes = new ApiRes<Object>();
-			apiRes.setError(true);
-			apiRes.setErrorReason("Vui lòng đăng nhập");
-			return ResponseEntity.ok(apiRes);
-		}
-		return ResponseEntity.ok(objBL.updateEmp(employeeReq, true));
-	}
+    @RequestMapping(value = "/update", method = RequestMethod.POST, consumes = "application/json", produces = "application/json; charset=utf-8")
+    public ResponseEntity<Object> updateEmp(@RequestBody EmployeeReq employeeReq, HttpSession session) {
+        if (!EmployeeDA.GetDataSource(session)) {
+            ApiRes<Object> apiRes = new ApiRes<Object>();
+            apiRes.setError(true);
+            apiRes.setErrorReason("Vui lòng đăng nhập");
+            return ResponseEntity.ok(apiRes);
+        }
+        return ResponseEntity.ok(objBL.updateEmp(employeeReq, true));
+    }
 
+    @GetMapping("/audit")
+    public ResponseEntity<Object> getAudit(HttpSession session) {
+        if (!EmployeeDA.GetDataSource(session)) {
+            ApiRes<Object> apiRes = new ApiRes<Object>();
+            apiRes.setError(true);
+            apiRes.setErrorReason("Vui lòng đăng nhập");
+            return ResponseEntity.ok(apiRes);
+        }
+        return ResponseEntity.ok(objBL.audits());
+    }
 }
--- src/main/java/com/employeemanagement/app/service/EmployeeService.java
+++ src/main/java/com/employeemanagement/app/service/EmployeeService.java
@@ -1,135 +1,143 @@
 package com.employeemanagement.app.service;
 
-import java.util.List;
-
-import com.employeemanagement.app.dao.DatabaseConfig;
-import com.employeemanagement.app.request.LoginReq;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
 import com.employeemanagement.app.dao.EmployeeDA;
+import com.employeemanagement.app.entities.Audit;
 import com.employeemanagement.app.entities.Employee;
 import com.employeemanagement.app.helpers.ApiRes;
 import com.employeemanagement.app.helpers.ValidationHelper.Result;
 import com.employeemanagement.app.request.EmployeeReq;
 import com.employeemanagement.app.request.FilterReq;
+import com.employeemanagement.app.request.LoginReq;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
-import com.employeemanagement.app.service.*;
+import java.util.List;
 
 @Service
 public class EmployeeService {
 
-	@Autowired
-	private HandlerBL handlerBL;
-	@Autowired
-	private EmployeeDA employeeDA;
+    @Autowired
+    private HandlerBL handlerBL;
+    @Autowired
+    private EmployeeDA employeeDA;
 
-	public ApiRes<Object> getlist(FilterReq req) {
-		ApiRes<Object> apiRes = new ApiRes<Object>();
-		try {
-			List<Employee> employees = employeeDA.getList(req.getKeyword());
-			apiRes.setObject(employees);
-		} catch (Exception e) {
-			apiRes.setError(true);
-			apiRes.setErrorReason(e.getMessage());
-		}
-		return apiRes;
-	}
+    public ApiRes<Object> getlist(FilterReq req) {
+        ApiRes<Object> apiRes = new ApiRes<Object>();
+        try {
+            List<Employee> employees = employeeDA.getList(req.getKeyword());
+            apiRes.setObject(employees);
+        } catch (Exception e) {
+            apiRes.setError(true);
+            apiRes.setErrorReason(e.getMessage());
+        }
+        return apiRes;
+    }
 
-	public ApiRes<Object> getInfor(EmployeeReq req) {
-		ApiRes<Object> apiRes = new ApiRes<Object>();
-		try {
-			Employee objEmployee = employeeDA.getInfor(req.getId());
-			apiRes.setObject(objEmployee);
-		} catch (Exception e) {
-			apiRes.setError(true);
-			apiRes.setErrorReason(e.getMessage());
-		}
-		return apiRes;
-	}
+    public ApiRes<Object> getInfor(EmployeeReq req) {
+        ApiRes<Object> apiRes = new ApiRes<Object>();
+        try {
+            Employee objEmployee = employeeDA.getInfor(req.getId());
+            apiRes.setObject(objEmployee);
+        } catch (Exception e) {
+            apiRes.setError(true);
+            apiRes.setErrorReason(e.getMessage());
+        }
+        return apiRes;
+    }
 
-	public ApiRes<Object> add(EmployeeReq req) {
-		ApiRes<Object> apiRes = new ApiRes<Object>();
-		try {
-			employeeDA.add(req);
-			apiRes.setObject(true);
-		} catch (Exception e) {
-			apiRes.setError(true);
-			apiRes.setErrorReason(e.getMessage());
-		}
-		return apiRes;
-	}
+    public ApiRes<Object> add(EmployeeReq req) {
+        ApiRes<Object> apiRes = new ApiRes<Object>();
+        try {
+            employeeDA.add(req);
+            apiRes.setObject(true);
+        } catch (Exception e) {
+            apiRes.setError(true);
+            apiRes.setErrorReason(e.getMessage());
+        }
+        return apiRes;
+    }
 
-	/*
-	 * public ApiRes<Object> update(EmployeeReq req) { ApiRes<Object> apiRes = new
-	 * ApiRes<Object>(); try { employeeDA.update(req); apiRes.setObject(true); }
-	 * catch (Exception e) { apiRes.setError(true);
-	 * apiRes.setErrorReason(e.getMessage()); } return apiRes; }
-	 */
+    /*
+     * public ApiRes<Object> update(EmployeeReq req) { ApiRes<Object> apiRes = new
+     * ApiRes<Object>(); try { employeeDA.update(req); apiRes.setObject(true); }
+     * catch (Exception e) { apiRes.setError(true);
+     * apiRes.setErrorReason(e.getMessage()); } return apiRes; }
+     */
 
-	public ApiRes<Object> login(LoginReq req, HttpSession session) {
-		ApiRes<Object> apiRes = new ApiRes<Object>();
-		try {
-			DataSource dataSource = employeeDA.connect(req);
-			if (dataSource == null)
-				apiRes = handlerBL.translateError(Result.login_fail, "Đăng nhập thất bại");
-			else {
-				session.setAttribute("datasource", dataSource);
-				apiRes.setObject(true);
-				apiRes.setError(false);
-			}
-		} catch (Exception e) {
-			apiRes = handlerBL.translateError(Result.login_fail, "Đăng nhập thất bại");
-		}
-		return apiRes;
+    public ApiRes<Object> login(LoginReq req, HttpSession session) {
+        ApiRes<Object> apiRes = new ApiRes<Object>();
+        try {
+            DataSource dataSource = employeeDA.connect(req);
+            if (dataSource == null)
+                apiRes = handlerBL.translateError(Result.login_fail, "Đăng nhập thất bại");
+            else {
+                session.setAttribute("datasource", dataSource);
+                apiRes.setObject(true);
+                apiRes.setError(false);
+            }
+        } catch (Exception e) {
+            apiRes = handlerBL.translateError(Result.login_fail, "Đăng nhập thất bại");
+        }
+        return apiRes;
 
-	}
+    }
 
-	public ApiRes<Object> logout(HttpSession session) {
-		ApiRes<Object> apiRes = new ApiRes<Object>();
-		try {
-			session.removeAttribute("dataSource");
-			apiRes.setObject(true);
-			apiRes.setError(false);
-		} catch (Exception e) {
-			apiRes = handlerBL.translateError(Result.login_fail, "Đăng xuất thất bại");
-		}
-		return apiRes;
-	}
-	
-	public ApiRes<Object> delEmp(int id) {
-		ApiRes<Object> apiRes = new ApiRes<Object>();
-		try {
-			employeeDA.delEmp(id);
-			apiRes.setObject(true);
-		} catch (Exception e) {
-			apiRes.setError(true);
-			apiRes.setErrorReason(e.getMessage());
-		}
-		return apiRes;
-	}
-	
-	/*
-	 * public ApiRes<Object> getEmpById(int id){ ApiRes<Object> apiRes= new
-	 * ApiRes<Object>(); try { Employee employee= employeeDA.getInfor(id);
-	 * apiRes.setObject(employee); } catch (Exception e) { e.printStackTrace();
-	 * apiRes.setError(true); apiRes.setErrorReason(e.getMessage()); } return
-	 * apiRes; }
-	 */
-	
-	public ApiRes<Object> updateEmp(Employee employee, boolean isUpdate){
-		ApiRes<Object> apiRes= new ApiRes<Object>();
-		try {
-			employeeDA.update(employee, isUpdate);
-			apiRes.setObject(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-			apiRes.setError(true);
-			apiRes.setErrorReason(e.getMessage());
-		}
-		return apiRes;
-	}
+    public ApiRes<Object> logout(HttpSession session) {
+        ApiRes<Object> apiRes = new ApiRes<Object>();
+        try {
+            session.removeAttribute("dataSource");
+            apiRes.setObject(true);
+            apiRes.setError(false);
+        } catch (Exception e) {
+            apiRes = handlerBL.translateError(Result.login_fail, "Đăng xuất thất bại");
+        }
+        return apiRes;
+    }
+
+    public ApiRes<Object> delEmp(int id) {
+        ApiRes<Object> apiRes = new ApiRes<Object>();
+        try {
+            employeeDA.delEmp(id);
+            apiRes.setObject(true);
+        } catch (Exception e) {
+            apiRes.setError(true);
+            apiRes.setErrorReason(e.getMessage());
+        }
+        return apiRes;
+    }
+
+    /*
+     * public ApiRes<Object> getEmpById(int id){ ApiRes<Object> apiRes= new
+     * ApiRes<Object>(); try { Employee employee= employeeDA.getInfor(id);
+     * apiRes.setObject(employee); } catch (Exception e) { e.printStackTrace();
+     * apiRes.setError(true); apiRes.setErrorReason(e.getMessage()); } return
+     * apiRes; }
+     */
+
+    public ApiRes<Object> updateEmp(Employee employee, boolean isUpdate) {
+        ApiRes<Object> apiRes = new ApiRes<Object>();
+        try {
+            employeeDA.update(employee, isUpdate);
+            apiRes.setObject(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            apiRes.setError(true);
+            apiRes.setErrorReason(e.getMessage());
+        }
+        return apiRes;
+    }
+
+    public ApiRes<Object> audits() {
+        ApiRes<Object> apiRes = new ApiRes<Object>();
+        try {
+            List<Audit> audits = employeeDA.getAudits();
+            apiRes.setObject(audits);
+        } catch (Exception e) {
+            apiRes.setError(true);
+            apiRes.setErrorReason(e.getMessage());
+        }
+        return apiRes;
+    }
 }
