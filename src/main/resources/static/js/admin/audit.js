a48c09fa7e12d66f473f4dada18f539501070bdc
NguyenVanPhap <nguyenvanphap013@gmail.com>
2022-01-11 17:14:59 +0700

    add logout

--- src/main/resources/static/js/admin/audit.js
+++ src/main/resources/static/js/admin/audit.js
@@ -1,36 +1,64 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    // load first when coming page
-    ajaxGet();
+	// load first when coming page
+	ajaxGet();
+	$(document).on('click', '#btnLogout', function(event) {
+		event.preventDefault();
+		ajaxLogout();
+	});
+	function ajaxLogout() {
+		var data = {};
+		// do post
+		$.ajax({
+			async: false,
+			type: "GET",
+			contentType: "application/json",
+			url: "http://localhost:8080/api/employee/logout",
+			enctype: 'application/json',
+			success: function(response) {
+				if (response.object == true) {
 
-    function ajaxGet() {
-        $.ajax({
-            type: "GET",
-            url: "http://localhost:8080/api/employee/audit",
-            success: function (result) {
-                if (result.error == true) {
-                    alert(result.toastMessage);
-                } else {
-                    console.log(result)
-                    $(".taiKhoanTable tbody").empty();
-                    $.each(result.object, function (i, audit) {
-                        var auditRow = '<tr>' +
-                            '<td>' + audit.username + '</td>' +
-                            '<td>' + audit.obj + '</td>' +
-                            '<td>' + audit.action + '</td>' +
-                            '<td>' + audit.timeStamp + '</td>';
-                        $('.taiKhoanTable tbody').append(auditRow);
-                    });
+					window.location.href = "/"
+				}
+				else {
+					alert("ERROR: ", response.toastMessage);
+				}
+			},
+			error: function(e) {
+				alert("Error!")
+				console.log("ERROR: ", e);
+			}
+		});
 
-                }
+	}
+	function ajaxGet() {
+		$.ajax({
+			type: "GET",
+			url: "http://localhost:8080/api/employee/audit",
+			success: function(result) {
+				if (result.error == true) {
+					alert(result.toastMessage);
+				} else {
+					console.log(result)
+					$(".taiKhoanTable tbody").empty();
+					$.each(result.object, function(i, audit) {
+						var auditRow = '<tr>' +
+							'<td>' + audit.username + '</td>' +
+							'<td>' + audit.obj + '</td>' +
+							'<td>' + audit.action + '</td>' +
+							'<td>' + audit.timeStamp + '</td>';
+						$('.taiKhoanTable tbody').append(auditRow);
+					});
 
-            },
-            error: function (e) {
-                if (e.error == true) {
-                    alert(result.toastMessage);
-                }
-                console.log(e)
-            }
-        });
-    };
+				}
+
+			},
+			error: function(e) {
+				if (e.error == true) {
+					alert(result.toastMessage);
+				}
+				console.log(e)
+			}
+		});
+	};
 });
